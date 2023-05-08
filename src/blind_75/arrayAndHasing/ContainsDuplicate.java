7f41e0b0477c6f6206a13841b85da4d943ed8ab2
SEO-J17 <qw4643987@naver.com>
2023-05-09 02:13:50 +0900

    solved 05-09

---
+++ src/blind_75/arrayAndHasing/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+package blind_75.arrayAndHasing;
+
+import java.util.*;
+
+// 217
+public class ContainsDuplicate {
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i + 1]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
