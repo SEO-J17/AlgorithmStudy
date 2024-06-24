4d96852f4290e0ee50c2e656cf53bad086cf534b
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2024-06-24 13:12:17 +0900

    [Silver IV] Title: 제로, Time: 244 ms, Memory: 23112 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                stack.push(num);
+            } else {
+                stack.pop();
+            }
+        }
+
+        int answer = 0;
+        while (!stack.isEmpty()) {
+            answer += stack.pop();
+        }
+
+        System.out.println(answer);
+    }
+}
