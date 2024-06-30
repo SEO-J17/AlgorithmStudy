712298d3cfa206ffd38f3b75b343a99542148fea
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2024-06-30 18:31:51 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 16872 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,85 +1,84 @@
-import java.io.*;
-import java.util.*; 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	static char map [][];
-	static char map2 [][];
-	static boolean visit1[][];
-	static boolean visit2[][];
-	static int dx[] = {0,0,1,-1};
-	static int dy[] = {1,-1,0,0};
-	static int normal_cnt,abnormal_cnt,n;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		
-		map = new char [n][n];			//정상맵
-		map2 = new char [n][n];			//색약맵
-		visit1 = new boolean [n][n];
-		visit2 = new boolean [n][n];
-		
-		for(int i = 0; i < n; i++) {
-			String temp = br.readLine();
-			for(int j = 0; j < n; j++) {
-					map[i][j] = temp.charAt(j);
-					if(temp.charAt(j) == 'G') {
-						map2[i][j] = 'R';
-					}else
-						map2[i][j] = temp.charAt(j);
-			}
-		}
-		
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				if(visit1[i][j] != true) {		//정상
-					normal_dfs(i,j);
-					normal_cnt++;
-				}
-				
-				if(visit2[i][j] != true) {		//색약
-					abnormal_dfs(i,j);
-					abnormal_cnt++;
-				}
-				
-			}
-		}
-		
-		System.out.println(normal_cnt + " " + abnormal_cnt);
-		
-	}
-	
-	
-	private static void normal_dfs(int x1, int y1) {
-		visit1[x1][y1] = true;
-		
-		for(int i = 0; i < 4; i++) {
-			int xx = dx[i]+x1;
-			int yy = dy[i]+y1;
-			if(xx >= 0 && yy >= 0 && xx < n && yy < n) {
-				if((visit1[xx][yy] != true) && (map[x1][y1] == map[xx][yy])) {
-					normal_dfs(xx, yy);
-				}
-			}
-		}
-	}
+    static char arr[][];
+    static char arr2[][];
+    static int dx[] = {0, 0, -1, 1};
+    static int dy[] = {1, -1, 0, 0};
+    static int n;
+    static boolean visit[][];
+    static boolean visit2[][];
 
-	
-	private static void abnormal_dfs(int x2, int y2) {
-		visit2[x2][y2] = true;
-		
-		for(int i = 0; i < 4; i++) {
-			int xx = dx[i]+x2;
-			int yy = dy[i]+y2;
-			if(xx >= 0 && yy >= 0 && xx < n && yy < n) {
-				if(visit2[xx][yy] != true && map2[x2][y2] == map2[xx][yy]) {
-					abnormal_dfs(xx, yy);
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        arr = new char[n][n];
+        arr2 = new char[n][n];
 
 
+        visit = new boolean[n][n];
+        visit2 = new boolean[n][n];
 
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = str.charAt(j);
+                if (arr[i][j] == 'G') {
+                    arr2[i][j] = 'R';
+                } else {
+                    arr2[i][j] = arr[i][j];
+                }
+            }
+        }
 
+        int num = 0;
+        int num2 = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j]) {
+                    num++;
+                    calc(i, j, arr[i][j]);
+                }
+                if (!visit2[i][j]) {
+                    num2++;
+                    calc2(i, j, arr2[i][j]);
+                }
+            }
+        }
+
+        System.out.println(num + " " + num2);
+    }
+
+    private static void calc(int x, int y, char area) {
+        visit[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int xx = dx[i] + x;
+            int yy = dy[i] + y;
+
+            if (xx >= 0 && yy >= 0 && xx < n && yy < n) {
+                if (!visit[xx][yy] && arr[xx][yy] == area) {
+                    calc(xx, yy, area);
+                }
+            }
+        }
+    }
+
+    private static void calc2(int x, int y, char area) {
+        visit2[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int xx = dx[i] + x;
+            int yy = dy[i] + y;
+
+            if (xx >= 0 && yy >= 0 && xx < n && yy < n) {
+                if (!visit2[xx][yy] && arr2[xx][yy] == area) {
+                    calc2(xx, yy, area);
+                }
+            }
+        }
+    }
 }
