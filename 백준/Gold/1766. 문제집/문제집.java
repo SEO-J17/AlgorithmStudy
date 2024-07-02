298c093e015475cd02f8252c28f40a1637c7389a
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2024-07-02 10:44:59 +0900

    [Gold II] Title: 문제집, Time: 500 ms, Memory: 43292 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,43 +1,46 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int arr[] = new int[n + 1];
-        ArrayList<Integer> list[] = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
+        int d[] = new int[n + 1];
+        List<Integer> list[] = new List[n + 1];
+
+        for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int node1 = Integer.parseInt(st.nextToken());
-            int node2 = Integer.parseInt(st.nextToken());
-            list[node1].add(node2);
-            arr[node2]++;
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            d[end]++;
+            list[start].add(end);
         }
 
         PriorityQueue<Integer> qu = new PriorityQueue<>();
         for (int i = 1; i <= n; i++) {
-            if (arr[i] == 0) {
+            if (d[i] == 0) {
                 qu.add(i);
             }
         }
-
+        
         StringBuilder sb = new StringBuilder();
         while (!qu.isEmpty()) {
-            int current = qu.poll();
-            sb.append(current).append(" ");
+            int node = qu.poll();
+            sb.append(node).append(" ");
 
-            for (int next : list[current]) {
-                arr[next]--;
-                if (arr[next] == 0) {
-                    qu.add(next);
+            for (int next : list[node]) {
+                d[next]--;
+                if (d[next] == 0) {
+                    qu.offer(next);
                 }
             }
         }
