3a0063ac52895c7a2617996213d1af4a73dbb76c
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2024-09-30 16:00:00 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.37 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,9 +4,7 @@ class Solution {
     int answer = 999999;
     ArrayList<Integer> list[];
     boolean visit[];
-    int nodeCnt1;
-    int nodeCnt2;
-    
+
     public int solution(int n, int[][] wires) {
         list = new ArrayList[n + 1];
         
@@ -24,21 +22,20 @@ class Solution {
         
         for(int i = 0 ; i < wires.length; i++){
             visit = new boolean[n + 1];
-            nodeCnt1 = calc(wires[i][0], wires[i][1], 1);
-            nodeCnt2 = calc(wires[i][1], wires[i][0], 1);
-            answer = Math.min(Math.abs(nodeCnt1 - nodeCnt2), answer);
+            int nodeCnt1 = calc(wires[i][0], wires[i][1]);
+            answer = Math.min(Math.abs(nodeCnt1 - (n - nodeCnt1)), answer);
         }        
         return answer;
     }
     
-    private int calc(int start, int unlink, int cnt){
+    private int calc(int start, int unlink){
         visit[start] = true;
         int count = 1;
         
         for(int next : list[start]){
             if(next != unlink){
                 if(!visit[next]){
-                    count += calc(next, unlink, cnt + 1);
+                    count += calc(next, unlink);
                 }
             }
         }
