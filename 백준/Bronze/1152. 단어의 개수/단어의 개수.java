935d9d61c31b89a8e4095ddce8bcf6c2222dc469
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2024-12-04 22:41:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 240 ms, Memory: 20140 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(st.countTokens());
+    }
+}
